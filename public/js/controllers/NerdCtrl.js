1a4bdfe51a3adec492cebf8004e5faf4d36af818
Isha Saxena <isha.saxena@daffodilsw.com>
2018-05-05 15:04:46 +0530

    commiting deleted files

--- public/js/controllers/NerdCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('NerdCtrl', []).controller('NerdController', function($scope, $stateParams, $state) {
-	console.log("here in nerdctrl")
-	$scope.salarySlip = $stateParams.salarySlip;
-	console.log("$scope.salarySlip??", $scope.salarySlip);
-	$scope.goBack = function(){
-		$state.transitionTo('home');
-	}
-});
